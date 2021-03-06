13afae56cb2fe8875a9e325bf4f2f5945241ebf6
Leewonjong <david8318@naver.com>
2021-03-07 02:10:12 +0900

    3장 끝

---
+++ src/main/java/com/wonjong/book/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,5 @@
+package com.wonjong.book.springboot.domain.posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long>{
+}
